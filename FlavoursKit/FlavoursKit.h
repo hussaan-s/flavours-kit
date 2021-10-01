d624396cf4d36dae83f72832b92289d8af7095a0
Muhammad Hussaan Saeed <hussaan.s@gmail.com>
2021-10-01 23:47:17 +0500

    Initial Setup

---
+++ FlavoursKit/FlavoursKit.h
@@ -0,0 +1,18 @@
+//
+//  FlavoursKit.h
+//  FlavoursKit
+//
+//  Created by Hussaan S on 16/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlavoursKit.
+FOUNDATION_EXPORT double FlavoursKitVersionNumber;
+
+//! Project version string for FlavoursKit.
+FOUNDATION_EXPORT const unsigned char FlavoursKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlavoursKit/PublicHeader.h>
+
+
